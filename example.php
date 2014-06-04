f01a4ab51a6ad4f76a52887f71850bffaac1a06f
Boris Momčilović <boris.momcilovic@gmail.com>
2014-06-04 18:58:51 +0200

    example.php: init

---
+++ example.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$api = new DatingVIP\API\Client;
+$api->setUrl ('http://www.domain.com/api/');
+$api->setAuth ('username', 'p4$$w0rd');
+
+$co_reg = new DatingVIP\API\CoReg ($api);
+$result = $co_reg->getWebsiteGenders ();
+print_r ($result->get ());
+
+$result = $co_reg->getGeoFormats ('RS');
+print_r ($result->get ());
+
+$result = $co_reg->checkEmail ('example@domain.com');
+print_r ($result->get ());
+
+$result = $co_reg->checkUsername ('my_username');
+print_r ($result->get ());
+
+$result = $co_reg->zipAndCity ('US', '333');
+print_r ($result->get ());
+
